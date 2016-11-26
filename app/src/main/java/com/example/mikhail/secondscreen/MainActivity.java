1b6ef4708cae05a9e0add38cf770a3ec316a3b65
mikhail <mdonskoi9@gmail.com>
2016-11-26 21:08:35 +0200

    1

---
+++ app/src/main/java/com/example/mikhail/secondscreen/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mikhail.secondscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_second);
+
+        Button aboutButton = (Button)findViewById(R.id.button);
+        Button secondButton = (Button)findViewById(R.id.button2);
+
+        aboutButton.setOnClickListener(this);
+        secondButton.setOnClickListener(this);
+
+    }
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button: Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
+                break;
+            case R.id.button2:Intent intentSecond = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intentSecond);
+                break;
+        }
+
+//        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+//        startActivity(intent);
+
+//        Intent intentSecond = new Intent(MainActivity.this, SecondActivity.class);
+//        startActivity(intentSecond);
+        // TODO: 11/24/16 reassign correctly button Second
+
+    }
+
+
+}
